173e15760450ac05ad522532e29bf9432d3e0781
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-04-01 19:59:41 +0300

    Update constants.py

--- Scripts/week_1/constants.py
+++ Scripts/week_1/constants.py
@@ -2,3 +2,21 @@
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 600
 FPS = 30
+
+
+####------ Time Line animation ------####
+TIME_LINE_ANIMATION_BACKGROUND = "Assets\\Time_Axis_Animation\\Space_Background.png"
+TIME_LINE_AXIS_ARROW = "Assets\\Time_Axis_Animation\\Time_Axis_Arrow.png"
+SAND_CLOCK_FRAMES = ["Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_1.png",
+                     "Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_2.png",
+                     "Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_3.png",
+                     "Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_4.png",
+                     "Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_5.png",
+                     "Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_6.png",
+                     "Assets\\Time_Axis_Animation\\Sand_clock_ticking\\sand_clock_7.png"]
+SAND_CLOCK_SIZE = 80
+
+####------ Player ------####
+PLAYER_HEALTH = 100
+PLAYER_SPEED = 5
+PLAYER_IMG_PATH = "Assets\\idle\\idle1.png"
