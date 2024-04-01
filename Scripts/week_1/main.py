90b80d751eeafaf0f1cd1bfffe0517c607c20cd2
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-04-01 19:58:19 +0300

    Update main.py

--- Scripts/week_1/main.py
+++ Scripts/week_1/main.py
@@ -1,6 +1,7 @@
-from constants import *
 import sys
-import pygame
+from player import *
+from Pages_Manager import *
+from Gif_Object import *
 
 # Initialize Pygame
 pygame.init()
@@ -12,12 +13,18 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Time Travel -Voyage Through History')
 
+# Creating the player
+# the_player = player()
+
+# gif_object(screen, 50, 50, 0, 0, 0, SAND_CLOCK_FRAMES).add_on_screen()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    PagesManager.time_line_page(screen, 1, 0)
     # Update the display and cap the frame rate
     pygame.display.flip()
     clock.tick(FPS)
