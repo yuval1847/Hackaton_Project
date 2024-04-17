d9ba806a2e83b50158f76700d8d062b777f408a7
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2024-04-17 23:29:10 +0300

    Add files via upload

---
+++ Scripts/week_3/functions.py
@@ -0,0 +1,11 @@
+from constants import *
+import pygame
+
+
+# def add_text(screen, text, color, x_pos, y_pos, size):
+#     # The function get the screen, the text, the text color, the x and y position and the font size
+#     # The function add the text's surface the text's rectangle on the screen
+#     font = pygame.font.Font(FONT1, size)
+#     text_surface = font.render(text, True, color)
+#     text_rect = text_surface.get_rect(center=(x_pos, y_pos))
+#     screen.blit(text_surface, text_rect)
